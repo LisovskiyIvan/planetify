26938564ee6589e5a08db5feb59ac4c61aee1d43
Lisovskii Ivan <dnevanek@gmail.com>
2024-09-02 23:11:40 +0300

    backend frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./Components/ui/button"
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-3xl">
+        <Button>qewqweq</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
